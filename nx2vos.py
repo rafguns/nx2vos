8ce5c3c3e472b92672a97800cb3071273d4cb4ae
rafguns <raf.guns@uantwerpen.be>
2023-11-29 10:44:58 +0100

    Support using any node attribute
    Rather than expecting the user to convert their node attributes to what VOSviewer wants, we now convert it for them.

--- nx2vos.py
+++ nx2vos.py
@@ -8,18 +8,103 @@ You will typically want to use them both, especailly if you have node-level data
 """
 import csv
 import pathlib
-from collections.abc import Iterable
 
 import networkx as nx
 
 __version__ = "0.1"
 
 
+class Nx2VosError(ValueError):
+    pass
+
+
+def _to_inc_number(node_vals):
+    unique_vals = {val for _, val in node_vals}
+    vals2number = dict(zip(unique_vals, range(1, len(unique_vals) + 1), strict=True))
+    return [(n, vals2number[v]) for n, v in node_vals]
+
+
+def _is_numeric(val):
+    return isinstance(val, int | float)
+
+
+def _transform_weight_score(attr_dict):
+    tmp = {}
+    for keyword in ["weight", "score"]:
+        for attr in attr_dict.pop(keyword, []):
+            tmp[f"{keyword}<{attr}>"] = attr
+    return {**attr_dict, **tmp}
+
+
+def _prepare_attrs(G: nx.Graph, attr_dict: dict[str, str | None]):
+    attrs = []
+
+    # Leave out all unspecified attributes (None or empty iterable)
+    attr_dict = {k: v for k, v in attr_dict.items() if v}
+    # Transform weight and score so we can handle them the same as other attributes.
+    attr_dict = _transform_weight_score(attr_dict)
+
+    # Check: x and y need to occur together
+    if len(attr_dict.keys() & {"x", "y"}) == 1:
+        err = "Attributes 'x' and 'y' cannot occur separately"
+        raise Nx2VosError(err)
+
+    for vos_attr, nx_attr in attr_dict.items():
+        node_vals = G.nodes(data=nx_attr)
+
+        # Check 1: attribute needs to be defined for every node
+        if any(val is None for _, val in node_vals):
+            err = f"Attribute '{nx_attr}' not defined for all nodes"
+            raise Nx2VosError(err)
+        # Check 2: numeric values where applicable
+        if (
+            vos_attr in {"x", "y"} or vos_attr.startswith(("weight", "score"))
+        ) and not all(_is_numeric(val) for _, val in node_vals):
+            err = f"Attribute '{vos_attr}' requires numeric values"
+            raise Nx2VosError(err)
+        # Transform: transform cluster names to incrementing numbers
+        if vos_attr == "cluster":
+            node_vals = _to_inc_number(node_vals)
+            # Check 3: no more than 1000 clusters
+            if any(val > 1000 for _, val in node_vals):  # noqa: PLR2004
+                err = "VOSviewer does not support more than 1000 clusters"
+                raise Nx2VosError(err)
+
+        for n, val_to_write in node_vals:
+            G.nodes[n][vos_attr] = val_to_write
+        attrs.append(vos_attr)
+
+    return G, attrs
+
+
 def write_vos_map(
-    G: nx.Graph, fname: str | pathlib.Path, attrs: Iterable | None = None
+    G: nx.Graph,
+    fname: str | pathlib.Path,
+    *,
+    sublabel_attr: str | None = None,
+    description_attr: str | None = None,
+    url_attr: str | None = None,
+    x_attr: str | None = None,
+    y_attr: str | None = None,
+    cluster_attr: str | None = None,
+    weight_attrs: list[str] | None = None,
+    score_attrs: list[str] | None = None,
 ):
-    if attrs is None:
-        attrs = []
+    # Transform attributes to VOSviewer format
+    G, attrs = _prepare_attrs(
+        G,
+        {
+            "sublabel": sublabel_attr,
+            "description": description_attr,
+            "url": url_attr,
+            "x": x_attr,
+            "y": y_attr,
+            "cluster": cluster_attr,
+            "weight": weight_attrs,
+            "score": score_attrs,
+        },
+    )
+    # Write to file
     with open(fname, "w", newline="") as fh:
         writer = csv.writer(fh, delimiter="\t")
         writer.writerow(["id", "label", *attrs])
@@ -28,7 +113,7 @@ def write_vos_map(
 
 
 def write_vos_network(G: nx.Graph, fname: str | pathlib.Path):
-    nodes = dict(zip(G.nodes(), range(1, len(G) + 1)))
+    nodes = dict(zip(G.nodes(), range(1, len(G) + 1), strict=True))
     with open(fname, "w", newline="") as fh:
         writer = csv.writer(fh, delimiter="\t")
         for u, v, d in G.edges(data=True):
--- test_nx2vos.py
+++ test_nx2vos.py
@@ -1,40 +1,183 @@
+import re
+
 import networkx as nx
 import pytest
 
 import nx2vos
 
 
+# region fixtures
 @pytest.fixture()
-def simple_network():
+def G_simple():  # noqa: N802 (Function name should be lowercase)
     G = nx.Graph()
-    G.add_weighted_edges_from(
-        [("a", "b", 1), ("b", "c", 2), ("a", "c", 3)]
-    )
-    # assign each node to cluster 1 or 2
-    for i, n in enumerate(G.nodes()):
-        G.nodes[n]["cluster"] = int(i % 2 == 0) + 1
+    G.add_weighted_edges_from([("a", "b", 1), ("b", "c", 2), ("a", "c", 3)])
 
     return G
 
-def test_write_vos_network(tmp_path, simple_network):
-    fname = tmp_path / "network.txt"
-    nx2vos.write_vos_network(simple_network, fname)
 
-    with open(fname) as fh:
+@pytest.fixture()
+def tmp_file(tmp_path):
+    return tmp_path / "file.txt"
+
+
+@pytest.fixture()
+def G_with_attrs(G_simple):  # noqa: N802 (Function name should be lowercase)
+    G = G_simple.copy()
+    for n in G:
+        G.nodes[n]["shorttext"] = n * 20
+        G.nodes[n]["longtext"] = n * 200
+
+        G.nodes[n]["urllike"] = f"http://x.co/{n}"
+
+        G.nodes[n]["smallint"] = 5
+        G.nodes[n]["largeint"] = 50_000
+
+        G.nodes[n]["float"] = 5.0
+
+    return G
+
+
+# endregion
+
+
+def test_write_vos_network(tmp_file, G_simple):
+    nx2vos.write_vos_network(G_simple, tmp_file)
+
+    with open(tmp_file) as fh:
         contents = list(fh)
 
     expected = {"1\t2\t1\n", "1\t3\t3\n", "2\t3\t2\n"}
     assert set(contents) == expected
 
 
-def test_write_vos_map(tmp_path, simple_network):
-    fname = tmp_path / "map.txt"
-    nx2vos.write_vos_map(simple_network, fname, attrs=["cluster"])
+def test_write_vos_map_noattrs(tmp_file, G_simple):
+    nx2vos.write_vos_map(G_simple, tmp_file)
 
-    with open(fname) as fh:
+    with open(tmp_file) as fh:
         contents = list(fh)
 
-    expected_header = "id\tlabel\tcluster\n"
-    expected = {"1\ta\t2\n", "2\tb\t1\n", "3\tc\t2\n"}
+    expected_header = "id\tlabel\n"
+    expected = {"1\ta\n", "2\tb\n", "3\tc\n"}
     assert contents[0] == expected_header
     assert set(contents[1:]) == expected
+
+
+@pytest.mark.parametrize(
+    ("attribute_mapping", "expected"),
+    [
+        (
+            {"sublabel": "shorttext"},
+            {f"1\ta\t{'a' * 20}\n", f"2\tb\t{'b' * 20}\n", f"3\tc\t{'c' * 20}\n"},
+        ),
+        (
+            {"description": "longtext"},
+            {f"1\ta\t{'a' * 200}\n", f"2\tb\t{'b' * 200}\n", f"3\tc\t{'c' * 200}\n"},
+        ),
+        (
+            {"url": "urllike"},
+            {"1\ta\thttp://x.co/a\n", "2\tb\thttp://x.co/b\n", "3\tc\thttp://x.co/c\n"},
+        ),
+        (
+            {"x": "smallint", "y": "smallint"},
+            {"1\ta\t5\t5\n", "2\tb\t5\t5\n", "3\tc\t5\t5\n"},
+        ),
+        (
+            {"x": "float", "y": "float"},
+            {"1\ta\t5.0\t5.0\n", "2\tb\t5.0\t5.0\n", "3\tc\t5.0\t5.0\n"},
+        ),
+        (
+            {"cluster": "smallint"},
+            {"1\ta\t1\n", "2\tb\t1\n", "3\tc\t1\n"},
+        ),
+    ],
+)
+def test_write_vos_map_attribute(tmp_file, G_with_attrs, attribute_mapping, expected):
+    mapping = {f"{k}_attr": v for k, v in attribute_mapping.items()}
+    nx2vos.write_vos_map(G_with_attrs, tmp_file, **mapping)
+
+    with open(tmp_file) as fh:
+        contents = list(fh)
+
+    vos_attributes = "\t".join(attribute_mapping.keys())
+    expected_header = f"id\tlabel\t{vos_attributes}\n"
+    assert contents[0] == expected_header
+    assert set(contents[1:]) == expected
+
+
+@pytest.mark.parametrize(
+    ("attribute_mapping", "expected_pattern"),
+    [
+        ({"cluster": "shorttext"}, "[123]\t[abc]\t[123]\n"),
+        ({"cluster": "longtext"}, "[123]\t[abc]\t[123]\n"),
+    ],
+)
+def test_write_vos_map_cluster(
+    tmp_file, G_with_attrs, attribute_mapping, expected_pattern
+):
+    mapping = {f"{k}_attr": v for k, v in attribute_mapping.items()}
+    nx2vos.write_vos_map(G_with_attrs, tmp_file, **mapping)
+
+    with open(tmp_file) as fh:
+        contents = list(fh)
+
+    vos_attributes = "\t".join(attribute_mapping.keys())
+    expected_header = f"id\tlabel\t{vos_attributes}\n"
+    assert contents[0] == expected_header
+    for line in contents[1:]:
+        assert re.match(expected_pattern, line) is not None
+
+
+@pytest.mark.parametrize(
+    ("attribute_mapping", "expected_header", "expected_contents"),
+    [
+        (
+            {"weight": ["smallint", "largeint"]},
+            "id\tlabel\tweight<smallint>\tweight<largeint>\n",
+            {"1\ta\t5\t50000\n", "2\tb\t5\t50000\n", "3\tc\t5\t50000\n"},
+        ),
+        (
+            {"score": ["smallint", "largeint"]},
+            "id\tlabel\tscore<smallint>\tscore<largeint>\n",
+            {"1\ta\t5\t50000\n", "2\tb\t5\t50000\n", "3\tc\t5\t50000\n"},
+        ),
+        (
+            {"weight": ["smallint"], "score": ["largeint"]},
+            "id\tlabel\tweight<smallint>\tscore<largeint>\n",
+            {"1\ta\t5\t50000\n", "2\tb\t5\t50000\n", "3\tc\t5\t50000\n"},
+        ),
+    ],
+)
+def test_write_vos_map_weights_scores(
+    tmp_file, G_with_attrs, attribute_mapping, expected_header, expected_contents
+):
+    mapping = {f"{k}_attrs": v for k, v in attribute_mapping.items()}
+    nx2vos.write_vos_map(G_with_attrs, tmp_file, **mapping)
+
+    with open(tmp_file) as fh:
+        contents = list(fh)
+
+    assert contents[0] == expected_header
+    assert set(contents[1:]) == expected_contents
+
+
+def test_nonexisting_attr_raises(G_simple, tmp_file):
+    with pytest.raises(nx2vos.Nx2VosError):
+        nx2vos.write_vos_map(G_simple, tmp_file, sublabel_attr="foo")
+
+
+def test_x_y_attr_raises(G_with_attrs, tmp_file):
+    with pytest.raises(nx2vos.Nx2VosError):
+        nx2vos.write_vos_map(G_with_attrs, tmp_file, x_attr="smallint")
+    with pytest.raises(nx2vos.Nx2VosError):
+        nx2vos.write_vos_map(G_with_attrs, tmp_file, y_attr="smallint")
+
+
+def test_nonnumeric_weights_scores_raises(G_with_attrs, tmp_file):
+    with pytest.raises(nx2vos.Nx2VosError):
+        nx2vos.write_vos_map(
+            G_with_attrs, tmp_file, weight_attrs=["smallint", "shorttext"]
+        )
+    with pytest.raises(nx2vos.Nx2VosError):
+        nx2vos.write_vos_map(
+            G_with_attrs, tmp_file, score_attrs=["smallint", "shorttext"]
+        )
